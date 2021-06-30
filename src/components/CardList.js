bde0d90f1c93d2e7488eb09a8a6453fa519e5d76
KimHwaYoung <dalle0601@naver.com>
2021-07-01 00:02:24 +0900

    redux 카드 클릭시 상태변환 성공....

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,18 +1,44 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
-function clickCard(name) {
-    console.log(name)
+import * as actions from '../actions';
+
+function clickCard(props) {
+    // props.props.SET_CHOICE(props.props.choice)
 }
+
 function CardList(props) {
+    const dispatch = useDispatch();
+
+    const [localChoice, setLocalChoice] = useState(true);
+    
+    const choice = useSelector((state) => state.tarotFetch.choice);
+
+    console.log(choice);
+
+    const setchoice = useCallback(() => {
+        setLocalChoice(!localChoice);
+        dispatch(actions.SET_CHOICE());
+    }, [localChoice, dispatch]);
+
     const name = props.name;
     const content = props.content;
-
     return (
-        <div style={{width:"120px", height:"200px", backgroundColor:"red"}} onClick={() => clickCard({name})}>
+        <div style={{width:"120px", height:"200px", backgroundColor:"red"}} onClick={setchoice}>
             <div>{name}</div>        
             <div>{content}</div>
         </div>
     );
 }
+
+// const mapDispatchToProps = (dispatch) => ({
+//     SET_CHOICE: () => dispatch(actions.SET_CHOICE()),
+// });
+
+// const mapStateToProps = (state) => ({
+    
+//     chocie : state.tarotFetch.chocie
+// });
 
 export default CardList; 
--- src/reducers/tarotFetch.js
+++ src/reducers/tarotFetch.js
@@ -4,19 +4,36 @@ const initialState = {
     choice: true
 };
 
-export default function tarotFetch(state = initialState, action){
-    switch (action.type){
+const tarotFetch = (state = initialState, action) => {
+    const {type} = action;
+debugger
+    switch(type){
         case types.GET_CHOICE:
             return {
                 ...state,
             };
         case types.SET_CHOICE:
-            return {
-                ...state,
-                chocie: !state.choice
-            };
+            return Object.assign({}, state, {choice: !(state.choice)})
+            
 
         default:
             return state;
     }
 }
+
+export default tarotFetch
+// export default function tarotFetch(state = initialState, action){
+//     debugger
+//     switch (action.type){
+//         case types.GET_CHOICE:
+//             return {
+//                 ...state,
+//             };
+//         case types.SET_CHOICE:
+//             return Object.assign({}, state, {choice: !(state.choice)})
+            
+
+//         default:
+//             return state;
+//     }
+// }
